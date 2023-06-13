fe7eac9d2f41a8b2207df738b0860e101321d58a
Laxiss-UwU <dimitrijasica60600@gmail.com>
2023-06-13 13:57:35 +0200

    updates des fichiers

--- test1/app1.py
+++ test1/app1.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def home():
 	return ("hello")
 
-app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_HOST"] = "127.0.0.1"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "foo"
-app.config["MYSQL_DB"] = "liste"
+app.config["MYSQL_DB"] = "base2"
 mysql = MySQL(app)
 
 @app.route("/test")
@@ -23,10 +23,10 @@ def login():
 		return formulaire
 
 	if request.method == "POST":
-		name = request.form["id"]
-		age = request.form["name"]
+		name = request.form["name"]
+		age = request.form["age"]
 		cursor = mysql.connection.cursor()
-		cursor.execute(""" INSERT INTO liste VALUES(%s,%s)""",(id,name))
+		cursor.execute(""" INSERT INTO info_table(name,age) VALUES (%s,%s) """,(name,age))
 		mysql.connection.commit()
 		cursor.close()
 		return f"Done!!"
--- test1/run_debcli1.sh
+++ test1/run_debcli1.sh
@@ -1,1 +1,1 @@
-docker run -it --network=host -h=127.0.0.1 deb-cli-1 bash
+docker run --rm -it --network=host -h=127.0.0.1 deb-cli-1 bash
